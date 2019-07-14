9ff9ec2d42a344d86cf3b83abb43616854365e00
carmenpmoreno <carmenp.moreno@gmail.com>
2019-07-14 12:33:12 +0200

    fix error on input values and listener on inputs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const headerSection = document.querySelector('.header-section');
 const mainSection = document.querySelector('.main-section');
+const ingredientsSelected = [];
 
-// fetch para traer los datos de la receta
 fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-setas.json')
     .then(response => response.json())
     .then(data => {
@@ -10,7 +10,6 @@ fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-seta
             paintData(recipe)
         );
     })
-// función que pinta todo el main con datos del fetch
 function paintData(recipe) {
     paintTitle(recipe);
     paintButton('Seleccionar todo', headerSection);
@@ -19,8 +18,13 @@ function paintData(recipe) {
     const classPriceSection = 'section-price';
     createSection(recipe, classArticleSection);
     createSection(recipe, classPriceSection);
+
+    const ingredientInputs = document.querySelectorAll('.ingredient-input');
+    console.log(ingredientInputs);
+    for (const ingredientInput of ingredientInputs) {
+        ingredientInput.addEventListener('click', handleIngredientInput);
+    }
 }
-// pinto el título de la receta en el header
 function paintTitle(recipe) {
     const title = document.createTextNode(recipe.name);
     const titleSelector = document.createElement('h2');
@@ -28,7 +32,6 @@ function paintTitle(recipe) {
     titleSelector.appendChild(title);
     headerSection.appendChild(titleSelector);
 }
-// para pintar botones
 function paintButton(text, section) {
     const buttonSelector = document.createElement('button');
     const buttonText = document.createTextNode(text);
@@ -36,7 +39,6 @@ function paintButton(text, section) {
     buttonSelector.appendChild(buttonText);
     section.appendChild(buttonSelector);
 }
-// creo secciones
 function createSection(recipe, classList) {
     const sectionList = document.createElement('section');
     sectionList.classList.add(classList);
@@ -45,17 +47,16 @@ function createSection(recipe, classList) {
         createArticles(recipe, sectionList);
     } else {
         createPricestitles(sectionList, 'Subtotal: ');
-        createPricestitles(sectionList, 'Gastos de envío:');
-        createPricestitles(sectionList, 'Total:');
-        paintButton('Deseleccionar todo', sectionList);
+        createPricestitles(sectionList, 'Gastos de envío: ');
+        createPricestitles(sectionList, 'Total: ');
+        paintButton('Comprar ingredientes: ', sectionList);
     }
 }
-// creo la lista de ingredientes, cada uno en un article
 function createArticles(recipe, sectionList) {
     recipe.ingredients.map(ingredient => {
         const articleSelector = document.createElement('article');
         createLabel(ingredient, articleSelector);
-        if(ingredient.brand) {
+        if (ingredient.brand) {
             createArticlesubtitles(ingredient.brand, articleSelector, "");
         }
         createArticlesubtitles(ingredient.quantity, articleSelector, "");
@@ -66,16 +67,17 @@ function createArticles(recipe, sectionList) {
 function createLabel(ingredient, articleSelector) {
     const labelSelector = document.createElement('label');
     createInput(ingredient, labelSelector);
-    labelSelector.setAttribute('for', 'ingredient.product');
+    labelSelector.setAttribute('for', ingredient.product);
     const labelContent = document.createTextNode(ingredient.product);
     labelSelector.appendChild(labelContent);
     articleSelector.appendChild(labelSelector);
 }
 function createInput(ingredient, labelSelector) {
     const inputSelector = document.createElement('input');
-    inputSelector.setAttribute('id', 'ingredient.product');
+    inputSelector.setAttribute('id', ingredient.product);
+    inputSelector.setAttribute('class', 'ingredient-input');
     inputSelector.setAttribute('type', 'checkbox');
-    inputSelector.setAttribute('value', 'ingredient.product');
+    inputSelector.setAttribute('value', ingredient.product);
     inputSelector.setAttribute('name', 'ingredients');
     labelSelector.appendChild(inputSelector);
 }
@@ -85,10 +87,30 @@ function createArticlesubtitles(subtitle, articleSelector, unit) {
     subtitleSelector.appendChild(subtitleContent);
     articleSelector.appendChild(subtitleSelector);
 }
-// creo la lista de precios
 function createPricestitles(sectionList, title) {
     const titleSelector = document.createElement('h3');
     const titleContent = document.createTextNode(title);
     titleSelector.appendChild(titleContent);
     sectionList.appendChild(titleSelector);
 }
+// INTERACCIONES
+
+// guardar precio en array ingredientsSelected cada vez que se selecciona:
+function handleIngredientInput(event) {
+    console.log(event.currentTarget.value);
+}
+    // listener sobre todos los input (funcion paint data)
+
+    // toggle clase checked cuanto se hace "click" en uninput
+    // si checked -> push del ingredient.price a mi array ingredientsSelected
+    // si no -> eliminar precio del array...
+// pintar precio final en seccion precios
+    // ...
+
+
+// botón seleccionar todo
+    // listener botón
+    // click -> add checked en todos los input (y por tanto push de todos los precios)
+// botón deseleccionar todo
+    // listener botón
+    // click -> remove checked en todos los input (volver a array vacío)
